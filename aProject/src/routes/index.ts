9f70dd8c9d614dc727000748f54cddcdd3f5ee41
Vanessa <vanessabasillio@gmail.com>
2021-09-20 18:39:09 -0300

    Table creation migration

--- aProject/src/routes/index.ts
+++ aProject/src/routes/index.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 const routes = Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "eae mundo!" });
+routes.post("/", (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email
+  };
+
+  return response.json({user});
 });
 
 export default routes;
